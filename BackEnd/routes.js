ef8af0b143f3b0790648404606e1bf620b3ef5b9
Ooogeid <diegomb972@gmail.com>
2024-11-07 11:46:45 +0100

    Home basic

--- BackEnd/routes.js
+++ BackEnd/routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('./controllers/userController');
+const eventController = require('./controllers/eventsController');
 
 // Route to get all users
 router.get('/getAllUser', userController.getAllUsers);
 router.post('/addUser', userController.createUser);
+router.get('/getEvents', eventController.getAllEvents);
 
 module.exports = router;
