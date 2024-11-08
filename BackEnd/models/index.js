a91890ec0ce7e2d0f87b753bd1374e64ac471198
Ooogeid <diegomb972@gmail.com>
2024-11-08 13:44:54 +0100

    modif index

--- BackEnd/models/index.js
+++ BackEnd/models/index.js
@@ -10,11 +10,8 @@ const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+
+sequelize_object = new Sequelize(config.database, config.username, config.password, config);
 
 fs
   .readdirSync(__dirname)
@@ -27,7 +24,7 @@ fs
     );
   })
   .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    const model = require(path.join(__dirname, file))(sequelize_object, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
@@ -37,7 +34,7 @@ Object.keys(db).forEach(modelName => {
   }
 });
 
-db.sequelize = sequelize;
+db.sequelize_object = sequelize;
 db.Sequelize = Sequelize;
 
 module.exports = db;
