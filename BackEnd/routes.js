4d01a24126a27844132b952c587137e31b5706f1
Ooogeid <diegomb972@gmail.com>
2024-12-20 14:56:40 +0100

    WIP : ajout des middlewares

--- BackEnd/routes.js
+++ BackEnd/routes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const { validateRegister, validateLogin } = require('./middlewares/validator.js');
+const { verifyToken } = require('./middlewares/auth.js');
+
 const userController = require('./controllers/userController');
 const eventController = require('./controllers/eventsController');
 
 // Route to get all users
-router.get('/getAllUser', userController.getAllUsers);
+router.get('/getAllUser', validateRegister, userController.getAllUsers);
 router.post('/addUser', userController.createUser);
 router.get('/getEvents', eventController.getAllEvents);
 
