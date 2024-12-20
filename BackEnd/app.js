37b1fed69fbb8c6d0d4aa273ba2d5a2d0f4f391c
PierreSc <pierre.schiavon@gmail.com>
2024-12-20 15:03:06 +0100

    commit swagger

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,16 +1,18 @@
 const express = require('express');
-const sequelize = require('./models').sequelize_object;  
+const sequelize = require('./models').sequelize;  
 const app = express();
 const userRoutes = require('./routes');
 const configureMiddleware = require('./middleware/middleware');
+const swaggerUi = require("./swagger/swagger");
+const swaggerSetup = require("./swagger/swagger").swaggerSetup;
 
 configureMiddleware(app);
 
 // Test database connection
 async function testDBConnection() {
   try {
-    await sequelize_object.authenticate({alter:true});
-    await sequelize_object.sync();
+    await sequelize.authenticate();
+    await sequelize.sync();
     console.log('Connexion réussie à la base de données PostgreSQL !');
   } catch (error) {
     console.error('Impossible de se connecter à la base de données :', error);
@@ -21,8 +23,10 @@ testDBConnection();
 
 // Use the user routes
 app.use('/', userRoutes);
+app.use("/docs", swaggerUi, swaggerSetup);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  console.log(`http://localhost:${PORT}/docs`);
 });
