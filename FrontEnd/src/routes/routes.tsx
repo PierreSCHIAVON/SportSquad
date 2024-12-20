55ab05e82831462f00a6a6188247a83f09c7a9b8
DouglasQuashie <idotetudes@gmail.com>
2024-12-20 14:25:16 +0100

    ajout landing et sa route

--- FrontEnd/src/routes/routes.tsx
+++ FrontEnd/src/routes/routes.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import HomePage from '../pages/Home';
+import LandingPage from '../pages/LandingPage';
 
 const routes = () => {
     return (
         <Routes>
-            <Route path="/" element={<HomePage />} />
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/home" element={<HomePage />} />
         </Routes>
     );
 };
