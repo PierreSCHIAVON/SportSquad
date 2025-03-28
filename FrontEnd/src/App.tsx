27784d3ac57e508c631b2d125c03ea25a912512e
Joninho11 <dahjonathan28@gmail.com>
2025-03-28 15:15:46 +0100

    Redirection is working

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,34 +1,44 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import "./App.css";
 import Layout from "./Components/Layout.tsx";
 import Body from "./Components/Layout/Body.tsx";
 import About from "./Components/Layout/About.tsx";
 import ConditionsUtilisation from "./Components/ConditionsUtilisation.tsx";
 import SearchPage from "./Components/Search.tsx";
-import ProfilUser from './Components/ProfilUser.tsx';
+import ProfilUser from "./Components/ProfilUser.tsx";
 import LoginPage from "./Components/Login";
 import AdditionalInfo from "./Components/Additional-info";
-import PrivateRoute from "./Components/PrivateRoutes"; // Import du composant
+import PrivateRoute from "./Components/PrivateRoutes";
+import LandingPage from "./Components/LandingPage.tsx";
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Routes accessibles sans authentification */}
+        {/* Route de la landing page */}
+        <Route path="/" element={<LandingPage />} />
+
+        {/* Route de login */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/login/additional-info" element={<AdditionalInfo />} />
 
         {/* Routes protégées */}
         <Route element={<PrivateRoute />}>
-          <Route path="/" element={<Layout />}>
+          <Route path="/dashboard" element={<Layout />}>
             <Route index element={<Body />} />
             <Route path="about" element={<About />} />
             <Route path="search" element={<SearchPage />} />
             <Route path="profil/:id" element={<ProfilUser />} />
-            <Route path="conditions-utilisation" element={<ConditionsUtilisation />} />
+            <Route
+              path="conditions-utilisation"
+              element={<ConditionsUtilisation />}
+            />
           </Route>
-          
         </Route>
+
+        {/* Redirection par défaut */}
+        <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </BrowserRouter>
   );
--- FrontEnd/src/Components/Login.tsx
+++ FrontEnd/src/Components/Login.tsx
@@ -1,92 +1,95 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { useState, useEffect } from 'react';
-import api from '../api/Api';
-import { useNavigate } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState, useEffect } from "react";
+import api from "../api/Api";
+import { useNavigate } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const LoginPage = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState<string | null>(null);
-    const [isLogin, setIsLogin] = useState(true); // Gère l'affichage du formulaire
-    const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState<string | null>(null);
+  const [isLogin, setIsLogin] = useState(true); // Gère l'affichage du formulaire
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            navigate('/');
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      navigate("/dashboard");
+    }
+  }, [navigate]);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError(null);
+
+    if (password.length < 6) {
+      setError("Le mot de passe doit contenir au moins 6 caractères.");
+      return;
+    }
+
+    try {
+      const endpoint = isLogin ? "/login" : "/register";
+      const response = await api.post(endpoint, { email, password });
+
+      if (isLogin) {
+        localStorage.setItem("token", response.data.token);
+        if (response.data.newAccount) {
+          navigate("login/additional-info", { state: { email } });
         }
-    }, [navigate]);
+      } else {
+        setIsLogin(true);
+      }
+    } catch (error: any) {
+      console.log(error.response);
+      setError(error.response?.data?.error || error.response?.data?.message);
+    }
+  };
 
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        setError(null);
+  return (
+    <div className="container d-flex justify-content-center align-items-center vh-100">
+      <div className="card p-4" style={{ width: "400px" }}>
+        <h2 className="text-center">
+          {isLogin ? "Se connecter" : "Créer un compte"}
+        </h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label className="form-label">Email</label>
+            <input
+              type="email"
+              className="form-control"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
 
-        if (password.length < 6) {
-            setError("Le mot de passe doit contenir au moins 6 caractères.");
-            return;
-        }
-        
-        try {
-            const endpoint = isLogin ? '/login' : '/register';
-            const response = await api.post(endpoint, { email, password });
-            
-            if (isLogin) {
-                localStorage.setItem('token', response.data.token);
-                if(response.data.newAccount) {
-                    navigate('login/additional-info', { state: { email } });
-                }
-            } else {
-                setIsLogin(true);
-            }
-        
-        } catch (error: any) { 
-            console.log(error.response);
-            setError(error.response?.data?.error || error.response?.data?.message);
-        }
-    };
+          <div className="mb-3">
+            <label className="form-label">Mot de passe</label>
+            <input
+              type="password"
+              className="form-control"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              minLength={6}
+              required
+            />
+          </div>
 
-    return (
-        <div className="container d-flex justify-content-center align-items-center vh-100">
-            <div className="card p-4" style={{ width: '400px' }}>
-                <h2 className="text-center">{isLogin ? 'Se connecter' : 'Créer un compte'}</h2>
-                <form onSubmit={handleSubmit}>
-                    <div className="mb-3">
-                        <label className="form-label">Email</label>
-                        <input
-                            type="email"
-                            className="form-control"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            required
-                        />
-                    </div>
+          {error && <div className="alert alert-danger">{error}</div>}
 
-                    <div className="mb-3">
-                        <label className="form-label">Mot de passe</label>
-                        <input
-                            type="password"
-                            className="form-control"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            minLength={6}
-                            required
-                        />
-                    </div>
-
-                    {error && <div className="alert alert-danger">{error}</div>}
-
-                    <button type="submit" className="btn btn-primary w-100">{isLogin ? 'Se connecter' : 'S\'inscrire'}</button>
-                </form>
-                <div className="text-center mt-3">
-                    <button className="btn btn-link" onClick={() => setIsLogin(!isLogin)}>
-                        {isLogin ? 'Créer un compte' : 'Déjà un compte ? Connectez-vous'}
-                    </button>
-                </div>
-            </div>
+          <button type="submit" className="btn btn-primary w-100">
+            {isLogin ? "Se connecter" : "S'inscrire"}
+          </button>
+        </form>
+        <div className="text-center mt-3">
+          <button className="btn btn-link" onClick={() => setIsLogin(!isLogin)}>
+            {isLogin ? "Créer un compte" : "Déjà un compte ? Connectez-vous"}
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
